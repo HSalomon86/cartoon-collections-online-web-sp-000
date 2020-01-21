b656db41946ce5ec20260f312260471848239768
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-21 01:36:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(element)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|c| c > 4}
+  calls.any? {|c| c.length > 4}
 end
 
 def find_the_cheese# code an argument here
